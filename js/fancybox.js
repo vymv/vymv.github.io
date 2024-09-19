91109f6df19e72e6196931118f65a98b4f347d95
vymv <925882085@qq.com>
2024-09-19 18:57:17 +0800

    Site updated: 2024-09-19 18:57:07

--- js/fancybox.js
+++ js/fancybox.js
@@ -6,16 +6,16 @@ $(function () {
   for (var i = 0; i < imgList.length; i++) {
     var $a = $(
       '<a href="' +
-        imgList[i].src +
-        '" data-fancybox="group" data-caption="' +
-        imgList[i].alt +
-        '" class="fancybox"></a>'
+      imgList[i].src +
+      '" data-fancybox="group" data-caption="' +
+      imgList[i].alt +
+      '" class="fancybox"></a>'
     )
     var alt = imgList[i].alt
     var $wrap = $(imgList[i]).wrap($a)
-    if (alt) {
-      $wrap.after('<div class="img-alt">' + alt + '</div>')
-    }
+    // if (alt) {
+    //   $wrap.after('<div class="img-alt">' + alt + '</div>')
+    // }
   }
 
   $().fancybox({
